696e0f96dd15ef07edbaf3d46c31d94767882c76
Pilar-cmd <mpilar.ribeiro20@gmail.com>
2026-04-10 15:24:52 -0300

    aaa

--- Assets/LixoSpawnerController.cs
+++ Assets/LixoSpawnerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class LixoSpawnerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 2 references
+    public float maximum;
+    public float fixedY;
+
+    // 1 reference
+    public float fixedZ;
+
+    // 1 reference
+    public float time;
+
+    // 1 reference
+    public GameObject lixo;
+
+    // 1 reference
+    public int maxPoints;
+
+    // 1 reference
+    public int points = 0;
+
+}   void Start()
+{
+    StartCoroutine(SpawnRoutine());
+}
 
     // Update is called once per frame
     void Update()
     {
         
     }
+    
+    IEnumerator SpawnRoutine() {
+    while (points < MaxPoints) {
+        Instantiate(Lixo,
+            new Vector3(Random.Range(-maximumX, maximumX + 1),
+                fixedY,
+                fixedZ),
+            Quaternion.identity);
+        yield return new WaitForSeconds(timer);
+    }
 }
+
