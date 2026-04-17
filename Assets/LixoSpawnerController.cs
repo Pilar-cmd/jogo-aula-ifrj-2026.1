ead9674e98521ca397cea5ac97841ddee8b6f02d
Pilar-cmd <mpilar.ribeiro20@gmail.com>
2026-04-17 15:18:55 -0300

    aaa

--- Assets/LixoSpawnerController.cs
+++ Assets/LixoSpawnerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LixoSpawnerController : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class LixoSpawnerController : MonoBehaviour
     public int maxPoints;
 
     public int points = 0;   
+
+    public TMP_Text pointsText;
 
     void Start() {
     StartCoroutine(SpawnRoutine());
@@ -36,6 +39,11 @@ public class LixoSpawnerController : MonoBehaviour
             Quaternion.identity);
         yield return new WaitForSeconds(time);
     }
+}
+
+public void AddToPoints(int value) {
+    points += value;
+    pointsText.text = "Points: " + points.ToString();
 }
 }
 
